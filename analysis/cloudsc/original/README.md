12ebcb9f3d4cec6b0e1ebf2fecb361ba3cd3f6b6
Marcin Copik <mcopik@gmail.com>
2023-04-06 23:25:24 +0200

    wip

--- analysis/cloudsc/original/README.md
+++ analysis/cloudsc/original/README.md
@@ -4,4 +4,5 @@
 python analyze_fortran.py ../../../data/cloudsc/original/fortran_cpu
 python analyze_c.py ../../../data/cloudsc/original/c_cpu
 python analyze_cuda.py ../../../data/cloudsc/original/c_cuda
+python analyze_openacc.py ../../../data/cloudsc/original/fortran_openacc
 ```
---
+++ analysis/cloudsc/original/analyze_openacc.py
@@ -0,0 +1,28 @@
+import sys
+import glob
+import os
+import pandas as pd
+
+data = []
+
+for f in glob.glob(os.path.join(sys.argv[1], "*.txt")):
+
+    vals = os.path.basename(f).split('.')
+    threads = int(vals[1])
+    size = int(vals[2])
+    nproma = int(vals[3])
+    rep = int(vals[4])
+
+    time = None
+    for line in open(f, 'r'):
+        if 'TOTAL' in line and len(line.split()) == 14:
+            time = line.split()[9]
+
+    if time is not None:
+        data.append([threads,size, nproma, rep, time])
+    else:
+        print('Missing time',f)
+
+df = pd.DataFrame(data, columns=['threads', 'size', 'nproma', 'repetition', 'execution_time'])
+df.to_csv('fortran_openacc.csv')
+
