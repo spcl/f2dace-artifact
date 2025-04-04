e0e8107245ce080662b8a33daef52a0e6337b690
Pratyai Mazumder <pmazumde@ault.cscs.ch>
2025-04-04 10:27:04 +0200

    update

--- cloudsc/dace/cpu/minirun.sh
+++ cloudsc/dace/cpu/minirun.sh
@@ -3,8 +3,8 @@ set -x
 
 cd dwarf-p-cloudsc/build/
 
-OMP_NUM_THREADS=1 valgrind --log-file=val.log ./bin/dwarf-cloudsc-fortran 1 1 4
+# OMP_NUM_THREADS=1 valgrind  --read-var-info=yes --track-origins=yes --log-file=val.log ./bin/dwarf-cloudsc-fortran 1 65536 128
 # OMP_NUM_THREADS=1 valgrind --vgdb-error=0 ./bin/dwarf-cloudsc-fortran 1 655 4
 # OMP_NUM_THREADS=1 gdb --args ./bin/dwarf-cloudsc-fortran 1 1 4
-# OMP_NUM_THREADS=1 ./bin/dwarf-cloudsc-fortran 1 655 4
+OMP_NUM_THREADS=1 ./bin/dwarf-cloudsc-fortran 1 65536 128
 
